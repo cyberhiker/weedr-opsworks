e26d9df06730fc4ca1749f649207b6c80f279ccf
Chris Burton <cburton@redeyetek.com>
2015-12-10 13:27:49 -0500

    Getting started with OpenLDAP

---
+++ postgres-hardening/metadata.rb
@@ -0,0 +1,19 @@
+name             "postgres-hardening"
+maintainer       "Christoph Hartmann"
+maintainer_email "chris@lollyrock.com"
+license          "Apache v2.0"
+description      "Installs and configures a secure posgres server"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.0.0"
+
+depends 'postgresql', '>= 3.4.0'
+
+recipe 'postgresql-hardening::default', 'calls hardening recipe'
+recipe 'postgresql-hardening::hardening', 'add hardening configuration postgresql server'
+
+supports 'amazon'
+supports 'redhat'
+supports 'centos'
+supports 'fedora'
+supports 'debian'
+supports 'ubuntu'
