7cce5254cf61cce1398247b6b5e369c95b81209c
Chris Burton <cburton@redeyetek.com>
2015-12-11 16:11:34 -0500

    Editing attributes

--- ejabberd/attributes/default.rb
+++ ejabberd/attributes/default.rb
@@ -2,7 +2,7 @@ node.default[:jabber_domain] = 'chat.weedr.io'
 node.default[:jabber_user] = 'ejabberd'
 node.default[:jabber_password] = '{]c3jvQ9WD3d@(ZEKH3dL7fc7H4Jr*(.jmBsgyA'
 
-node.default[:pgsql_hostname] = 'ec2.somewhere.in.rds.maybe'
+node.default[:pgsql_hostname] = 'localhost'
 node.default[:pgsql_databasename] = 'xmpp'
 node.default[:pgsql_username] = 'xmpp'
 node.default[:pgsql_password] = '{]c3jvQ9WD3d@(ZEKH3dL7fc7H4Jr*(.jmBsgyA'
