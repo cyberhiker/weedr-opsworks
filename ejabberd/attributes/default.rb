1c5628b7962854f4b728692ec728ea65b9e0d25e
Chris Burton <cburton@redeyetek.com>
2015-12-11 15:59:02 -0500

    Adding attributes

---
+++ ejabberd/attributes/default.rb
@@ -0,0 +1,8 @@
+node.default[:jabber_domain] = 'chat.weedr.io'
+node.default[:jabber_user] = 'ejabberd'
+node.default[:jabber_password] = '{]c3jvQ9WD3d@(ZEKH3dL7fc7H4Jr*(.jmBsgyA'
+
+node.default[:pgsql_hostname] = 'ec2.somewhere.in.rds.maybe'
+node.default[:pgsql_databasename] = 'xmpp'
+node.default[:pgsql_username] = 'xmpp'
+node.default[:pgsql_password] = '{]c3jvQ9WD3d@(ZEKH3dL7fc7H4Jr*(.jmBsgyA'
